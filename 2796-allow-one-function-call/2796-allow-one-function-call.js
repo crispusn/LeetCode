b3852cb44e6abbbd6b6a50dea9b9b500c1e011f0
Kaan Soylev <kaansoylev@gmail.com>
2023-07-12 04:59:29 +0300

    Time: 63 ms (30.36%), Space: 42.3 MB (8%) - LeetHub

--- 2796-allow-one-function-call/2796-allow-one-function-call.js
+++ 2796-allow-one-function-call/2796-allow-one-function-call.js
@@ -1,22 +1,11 @@
-/**
- * @param {Function} fn
- * @return {Function}
- */
-var once = function(fn) {
-    let res = false;
-    return function(...args){
-        if(!res){
-            res = true;
-            return fn(...args);
-        }
-        
-    }
-};
+function once(fn) {
+  let hasRun = false;
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+  return function(...args) {
+    if (!hasRun) {
+      hasRun = true;
+      return fn(...args);
+    }
+  };
+}
+
